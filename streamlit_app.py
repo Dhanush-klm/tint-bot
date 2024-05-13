e5965e3f11910b511ee51d8e761d9d2afaa0bf77
Dhanush-klm <167899716+Dhanush-klm@users.noreply.github.com>
2024-05-13 14:22:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
 api_key = st.secrets["HUGGINGFACE_API_KEY"]
